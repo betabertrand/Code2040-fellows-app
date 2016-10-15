86cce10364c97ea91e17d7e33e8158db58f53447
bertrand <btsp60@yahoo.com>
2016-10-15 00:12:56 -0400

    get api set

---
+++ main.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+	var info = {
+		"token" : "a14c730acf2c507c7462c5b711d88804",
+		"github" : "https://github.com/thenightowl00/Code2040-fellows-app"
+	}
+
+	$(".outer").text("testing.....");
+	$(".outer").text(info);
+	console.log(info);
+
+	/*$.ajax({
+		url:'http://challenge.code2040.org/api/register',
+		type: 'POST',
+		crossDomai"token" : "a14c730acf2c507c7462c5b711d88804",
+		"github" : "https://github.com/thenightowl00/Code2040-fellows-app"n: true,
+		data: JSON.stringify(info),
+		dataType: 'text'
+	}).done(function (data) {
+		$(".outer").text(data);
+		console.log(data)
+	});
+	*/
+
+
+	
+	$.post("http://challenge.code2040.org/api/register", {"token" : "a14c730acf2c507c7462c5b711d88804",
+		"github" : "https://github.com/thenightowl00/Code2040-fellows-app"}).done(function () {
+			alert("done!!!");
+		});
+
+
+
+
+
+
+
+
+
+});
